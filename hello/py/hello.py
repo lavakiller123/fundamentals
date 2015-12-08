1fa517159a7e51c5d7c2ebd3e482774ce8074964
Noah Brett <footballisawesome1012@gmail.com>
2015-12-07 20:32:19 -0500

    save

--- hello/py/hello.py
+++ hello/py/hello.py
@@ -15,29 +15,3 @@ def nyan(message):
     """print hello world in nyan"""
     while True:
         print(c.rc() + message, end= " ")
-
-if __name__ == '__main__':
-    import sys
-
-    option = ""
-    message = ""
-    who = "world"
-
-    if len(sys.argv) > 2:
-        option = sys.argv[1]
-        who = sys.argv[2]
-    elif len(sys.argv) == 2:
-        if sys.argv[1].startswith("-"):
-            option = sys.argv[1]
-        else:
-            who = sys.argv[1]
-
-    message = "Hello " + who
-
-    if option == "-n":
-        nyan(message)
-    if option == "-m":
-        multi(message)
-    else:
-        
-        hello(who)
